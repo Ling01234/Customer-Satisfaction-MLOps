b204d2d52bbe90085587eaa8d775bbb1362e8916
Ling Fei Zhang <lzhang133@gmail.com>
2024-02-20 20:42:06 -0500

    mlflow setup and experiment tracking

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,14 +1,34 @@
 import logging
-from zenml import step
 import pandas as pd
 from src.evaluation import MSE, R2, RMSE
 from sklearn.base import RegressorMixin
 
 from typing_extensions import Annotated
 from typing import Tuple
+import mlflow
+from zenml.client import Client
+from zenml import step
+
+experiment_tracker = Client().active_stack.experiment_tracker
+# # Get the client
+# client = Client()
+
+# # Check if there is an active stack
+# if client.active_stack is None:
+#     print("There is no active stack.")
+# else:
+#     print(f"The active stack is: {client.active_stack.name}")
+
+#     # Check if the active stack has an experiment tracker
+#     if client.active_stack.experiment_tracker is None:
+#         print("The active stack does not have an experiment tracker.")
+#     else:
+#         print(
+#             f"The experiment tracker is: {client.active_stack.experiment_tracker.name}")
+#         experiment_tracker = client.active_stack.experiment_tracker
 
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def evaluate_model(model: RegressorMixin,
                    X_test: pd.DataFrame,
                    y_test: pd.Series) -> Tuple[
@@ -20,12 +40,15 @@ def evaluate_model(model: RegressorMixin,
 
         mse_class = MSE()
         mse = mse_class.calculate_score(y_test, prediction)
+        mlflow.log_metric('mse', mse)
 
         r2_class = R2()
         r2 = r2_class.calculate_score(y_test, prediction)
+        mlflow.log_metric('r2', r2)
 
         rmse_class = RMSE()
         rmse = rmse_class.calculate_score(y_test, prediction)
+        mlflow.log_metric('rmse', rmse)
 
         return r2, rmse
 
--- steps/model_train.py
+++ steps/model_train.py
@@ -4,9 +4,13 @@ from zenml import step
 from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
 from .config import ModelNameConfig
+import mlflow
+from zenml.client import Client
+
+experiment_tracker = Client().active_stack.experiment_tracker
 
 
-@step
+@step(experiment_tracker=experiment_tracker.name)
 def train_model(X_train: pd.DataFrame,
                 X_test: pd.DataFrame,
                 y_train: pd.Series,
@@ -18,6 +22,7 @@ def train_model(X_train: pd.DataFrame,
     try:
         if config.model_name == 'LinearRegression':
             model = LinearRegressionModel()
+            mlflow.sklearn.autolog()
             trained_model = model.train(X_train=X_train, y_train=y_train)
             logging.info('Completed Model training')
             return trained_model
